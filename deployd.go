a807879f0d04f7cbcac794a588b6d7c4643048d4
Mihir Singh <git.service@mihirsingh.com>
2014-12-11 02:30:55 -0500

    Separated the code for unzipping downloads

--- deployd.go
+++ deployd.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"archive/zip"
 	"crypto/md5"
 	"fmt"
 	"io"
@@ -9,13 +8,12 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strconv"
-	"strings"
 	"time"
 
 	cachestore "github.com/citruspi/deployd/cache"
 	"github.com/citruspi/deployd/configuration"
+	"github.com/citruspi/deployd/unzip"
 )
 
 var (
@@ -150,7 +148,7 @@ func main() {
 			log.Fatal(err)
 		}
 
-		err = unzip(archivePath, unarchivedPath)
+		err = unzip.Unzip(archivePath, unarchivedPath)
 
 		if err != nil {
 			log.Fatal(err)
@@ -201,51 +199,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-// unzip function by http://stackoverflow.com/users/1129149/swtdrgn
-// http://stackoverflow.com/questions/20357223/easy-way-to-unzip-file-with-golang/24430720#24430720
-
-func unzip(src, dest string) error {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	for _, f := range r.File {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer rc.Close()
-
-		fpath := filepath.Join(dest, f.Name)
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(fpath, f.Mode())
-		} else {
-			var fdir string
-			if lastIndex := strings.LastIndex(fpath, string(os.PathSeparator)); lastIndex > -1 {
-				fdir = fpath[:lastIndex]
-			}
-
-			err = os.MkdirAll(fdir, f.Mode())
-			if err != nil {
-				log.Fatal(err)
-				return err
-			}
-			f, err := os.OpenFile(
-				fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-			defer f.Close()
-
-			_, err = io.Copy(f, rc)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
 }
